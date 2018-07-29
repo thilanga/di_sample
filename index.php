4ba7df424716fe4a5eec0360d6c4c12437e0763c
Thilanga Pitigala <thilanga.pitigala@digital360.com.au>
2018-07-29 22:17:17 +1000

    Updated to indenting

--- index.php
+++ index.php
@@ -3,8 +3,10 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Sample\MusicPlayer;
 use Sample\Services\Google\GoogleMusic;
+use Sample\Services\Spotify\SpotifyMusic;
 
-$musicService = new GoogleMusic();
+// $musicService = new GoogleMusic();
+$musicService = new SpotifyMusic();
+
 $musicPlayer = new MusicPlayer($musicService);
-
 $musicPlayer->play();

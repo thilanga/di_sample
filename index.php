3e2e5ef7f4d9a05a625aaedc25de555be3b1fe81
Thilanga Pitigala <thilanga.pitigala@digital360.com.au>
2018-07-29 22:24:53 +1000

    Updated to remove Laravel IOC container

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use Sample\MusicPlayer;
 use Sample\Services\Google\GoogleMusic;
 use Sample\Services\Spotify\SpotifyMusic;
 
-// $musicService = new GoogleMusic();
+$musicService = new GoogleMusic();
 $musicService = new SpotifyMusic();
 
 $musicPlayer = new MusicPlayer($musicService);

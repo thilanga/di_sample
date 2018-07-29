f4cfb41810cf6c0dda66dd1b461cbda230804a37
Thilanga Pitigala <thilanga.pitigala@digital360.com.au>
2018-07-30 00:45:16 +1000

    Updated to use IOC injection based on user names

--- index.php
+++ index.php
@@ -1,12 +1,24 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
+use Sample\Contracts\IMusicService;
 use Sample\MusicPlayer;
 use Sample\Services\Google\GoogleMusic;
 use Sample\Services\Spotify\SpotifyMusic;
 
-$musicService = new GoogleMusic();
-$musicService = new SpotifyMusic();
+// Create new IoC Container instance
+$container = new Illuminate\Container\Container;
 
-$musicPlayer = new MusicPlayer($musicService);
+$container->singleton(IMusicService::class, function ($app) {
+    switch (strtolower($_GET['user'])) {
+        case 'thilanga':
+            return new GoogleMusic();
+        case 'pittado':
+            return new SpotifyMusic();
+        default:
+            throw new Exception('No Music service been configured for this user');
+    }
+});
+
+$musicPlayer = new MusicPlayer($container->make(IMusicService::class));
 $musicPlayer->play();
